22783c6ad273eb6bec987bbc1206d8add9857d26
Vincent Bernat <vincent@bernat.im>
2015-03-24 23:46:23 +0100

    d/missing-sources: build jquery-ui.custom.js from libjs-jquery-ui.
    The previous change is not needed anymore. Functions are already
    enclosed in a pair of parentheses. However, it seems that the date
    picker needs to be appended now.

--- debian/create-jquery-ui-custom.sh
+++ debian/create-jquery-ui-custom.sh
@@ -1,24 +1,14 @@
 #!/bin/sh
 #
-# The program/js/jquery-ui-1.9.1.custom.js can't be shipped
+# The program/js/jquery-ui-1.10.4.custom.js can't be shipped
 # because we don't have the source for it.  So construct that
 # works the way roundcude wants it from the original sources.
 #
 # We actually just mung the minified source because it's closest
 # to what we want.
 #
-# This transformation was figured out by diffing the
-# jquery-ui-1.9.1.custom.js to the one shipped in the original
-# (ie unrepacked, ie not the Debian .orig.tar.gz) tarball.
-#
-# In brief, the transformation is to replace every global
-# function definition, which all have the form:
-#   function (arg ...) { ... } (jQuery),
-# with:
-#   (function (arg ...) { ... }) (jQuery);
+# We just append jquery-ui-accessible-datepicker.js
 
-unzip -p "${1}" "*/jquery-ui.min.js" |
-  sed '
-      s/}\((jQuery)\),\(jQuery.effects||\)\?\(function([a-z$]\)/})\1;\2(\3/g
-      s/})(jQuery),function(/})(jQuery);(function(/
-      s/}(jQuery);$/})(jQuery);/'
+cat \
+    /usr/share/javascript/jquery-ui/jquery-ui.min.js \
+    plugins/jqueryui/js/jquery-ui-accessible-datepicker.js
